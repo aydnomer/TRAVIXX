f4c6a77eb555da9b4f8cf0a2fec9f05366d307c0
aydnomer <aydnomrfrk@gmail.com>
2026-05-09 17:49:18 +0300

    Supabase bağlantısı kuruldu

---
+++ lib/core/constants/supabase_constants.dart
@@ -0,0 +1,5 @@
+class SupabaseConstants {
+  static const String supabaseUrl = 'https://zwbupljwhnnktgshpzpf.supabase.co';
+  static const String supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3YnVwbGp3aG5ua3Rnc2hwenBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMjg3MzcsImV4cCI6MjA5MzkwNDczN30.oBSaeh2lD0F3ECIoeX15Z1iVLj6TeNZwsQxJWmmuqHA';
+}
